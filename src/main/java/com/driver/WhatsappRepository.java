9292e15da7501757e8fa5c145a836ba835dc5428
gupta <guptasanchit041@gmail.com>
2023-01-28 15:59:37 +0530

    message

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -4,6 +4,11 @@ import java.util.*;
 
 import org.springframework.stereotype.Repository;
 
+import java.beans.ExceptionListener;
+import java.util.*;
+
+import org.springframework.stereotype.Repository;
+
 @Repository
 public class WhatsappRepository {
 
@@ -26,4 +31,104 @@ public class WhatsappRepository {
         this.customGroupCount = 0;
         this.messageId = 0;
     }
+    public String createUser(String name,String Phone_no)
+    {
+        if(userMobile.contains(Phone_no))
+        {
+            return "Already Present";
+        }
+        User u = new User(name,Phone_no);
+//        u.add(u);
+        userMobile.add(Phone_no);
+        return "Success";
+    }
+   public Group  createGroup(List<User> user)
+   {
+       if(user.size()<=1){
+
+       }
+       Group g ;
+       if(user.size()==2)
+       {
+            g= new Group(user.get(1).getName(),customGroupCount+1);
+           customGroupCount +=1;
+           groupUserMap.put(g,user);
+       }
+       else
+       {
+             g = new Group("Group"+(customGroupCount+1),customGroupCount+1);
+            customGroupCount++;
+            adminMap.put(g,user.get(1));
+            groupUserMap.put(g,user);
+       }
+       return g;
+   }
+   public int createMessage(String content)
+   {
+       Message m =new Message(messageId+1,content,new Date());
+       messageId++;
+       return m.getId();
+   }
+   public int sendMessage(Message message,User user,Group group) throws Exception
+   {
+       if(!groupUserMap.containsKey(group))
+       {
+           throw new Exception("Group does not exist");
+       }
+       boolean flag = false;
+       for( Group li : groupUserMap.keySet())
+       {
+           List<User> get = groupUserMap.get(li);
+           for(User u : get)
+           {
+               if(Objects.equals(u,user))
+               {
+                   flag = true;
+               }
+           }
+       }
+       if(flag == false)
+       {
+           throw new Exception("User is not part of group");
+       }
+       senderMap.put(message,user);
+       groupMessageMap.put(group,groupMessageMap.getOrDefault(group,new ArrayList<>()));
+       groupMessageMap.get(group).add(message);
+       return groupMessageMap.get(group).size();
+   }
+   public String changeAdmin(User approver, User user, Group group)throws Exception {
+        if(groupUserMap.containsKey(group))
+        {
+             throw new Exception("");
+        }
+       if(!adminMap.get(group).equals(approver))
+       {
+           throw new Exception("");
+       }
+         boolean flag = false;
+       for( Group li : groupUserMap.keySet())
+       {
+           List<User> get = groupUserMap.get(li);
+           for(User u : get)
+           {
+               if(Objects.equals(u,user))
+               {
+                   flag = true;
+               }
+           }
+       }
+       if(flag == false)
+       {
+           throw new Exception("User is not part of group");
+       }
+       adminMap.put(group,user);
+        return "SUCCESS";
+    }
+
+//    public int removeUser(User user) {
+//      return 1;
+//    }
+//    public String findMessage(Date start, Date end, int k) {
+//        return "";
+//    }
 }
